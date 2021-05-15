8825ecd24be0b28b9b86b002c7ae330f0431162d
felipe <felipemedina7878@gmail.com>
2021-05-15 18:21:39 -0500

    add react

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import InitialState from '../initialState';
+import {Helmet} from 'react-helmet'
 import Products from '../components/Products';
 const Home = () => {
   return (
     <>
+    <Helmet>
+      <title>Productos - Hamburgruesas</title>
+    </Helmet>
       <Products products={InitialState.products}></Products>
     </>
   );
--- src/containers/about.jsx
+++ src/containers/about.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
 import '../styles/components/About.scss'
 import hamburguer from '../assets/hamburguer.png'
 
@@ -7,6 +8,30 @@ const about = () =>{
 
     return(
         <>
+        <Helmet>
+            <title>Hamburgueseria Los Pintos</title>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:site" content="@felipemedinabel"/>
+            <meta name="twitter:creator" content="@felipemedinabel"/>
+            <meta name="twitter:title" content="Hamburgueseria Los Pintos"/>
+            <meta name="twitter:description" content="La mejor e-commerce de hamburguesas del mundo"/>
+            <meta
+            name="twitter:image"
+            content="https://cocheros.com.co/images/producto/pionera.png"
+            />
+            <meta property="og:title" content="Hamburgueseria Los Pintos"/>
+            <meta property="og:description" content="Hamburgueseria Los Pintos"/>
+            <meta
+            property="og:image"
+            content="https://cocheros.com.co/images/producto/pionera.png"
+            />
+            <meta property="og:url" content="https://hamburguer-store.web.app/" />
+            <meta property="og:site_name" content="Hamburgueseria Los Pintos" />
+            <meta property="og:locale" content="es_ES" />
+            <meta property="og:type" content="article" />
+            <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+
+        </Helmet>
         <main className="About">
             <section className="hamburguer-container">
 
